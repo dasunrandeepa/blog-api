083c5a3108e3b1813e8262e7e3698577a5a9a970
dasunR430 <128127298+dasunR430@users.noreply.github.com>
2025-08-05 23:59:13 +0530

    Add User model

--- src/models/user.ts
+++ src/models/user.ts
@@ -22,4 +22,13 @@ export interface IUser {
 /**
  * User Schema
  */
-const userSchema = new Schema<IUser>()
+const userSchema = new Schema<IUser>(
+    {
+        username: {
+            type: String,
+            required: [true, 'Username is required'],
+            maxlength: [20, 'Username must be less than 20 characters'],
+            unique: [true, 'Username must be unique']
+        }
+    }
+)
