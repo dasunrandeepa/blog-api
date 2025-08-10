855900a28041f133f0e280df26ec90b0bc281a10
dasunR430 <128127298+dasunR430@users.noreply.github.com>
2025-08-10 19:37:06 +0530

    Add login

--- src/routes/v1/auth.ts
+++ src/routes/v1/auth.ts
@@ -2,22 +2,94 @@
  * Node Modules
  */
 import { Router } from 'express';
+import { body } from 'express-validator';
+import bcrypt from 'bcrypt';
 
 /**
  * Controllers
  */
-import register from '@/controllers/v1/auth/register'
+import register from '@/controllers/v1/auth/register';
+import login from '@/controllers/v1/auth/login';
 
 /**
  * Middleware
  */
-
+import validationError from '@/middlewares/validationError';
 
 /**
  * Models
  */
+import User from '@/models/user';
+
 const router = Router();
 
-router.post('/register', register);
+router.post(
+    '/register', 
+    body('email')
+        .trim()
+        .notEmpty()
+        .withMessage('Email is Required')
+        .isLength({max: 50})
+        .withMessage('Email must be less than 50 characters')
+        .isEmail()
+        .withMessage('Invalid email address')
+        .custom(async (value) => {
+            const userExists = await User.exists({ email:value });
+            if (userExists){
+                throw new Error('User already exists.');
+            }
+        }), 
+    body('password')
+        .notEmpty()
+        .withMessage('Password is Required')
+        .isLength({min: 8})
+        .withMessage('Password must be atleast 8 characters long.'),
+    body('role')
+    .optional()
+    .isString()
+    .withMessage('Role must be a string.')
+    .isIn(['admin','user'])
+    .withMessage('Role must be either admin or user'),    
+    validationError,
+    register
+);
+
+router.post(
+    '/login',
+    body('email')
+        .trim()
+        .notEmpty()
+        .withMessage('Email is Required')
+        .isLength({max: 50})
+        .withMessage('Email must be less than 50 characters')
+        .isEmail()
+        .withMessage('Invalid email address'), 
+    body('password')
+        .notEmpty()
+        .withMessage('Password is Required')
+        .isLength({min: 8})
+        .withMessage('Password must be atleast 8 characters long.')
+        .custom(
+            async( value, { req }) => {
+                const { email } = req.body as { email: string };
+                const user = await User.findOne({ email })
+                .select('password')
+                .lean()
+                .exec();
+
+                if (!user) {
+                    throw new Error('User email or password is invalid.');
+                }
+
+                const passwordMatch = await bcrypt.compare(value, user.password);
+
+                if(!passwordMatch) {
+                    throw new Error('User email or password is invalid');
+                }
+            }
+        ),
+    validationError,
+    login
+);
 
 export default router;
