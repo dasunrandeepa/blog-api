d8fb7ca10ac43923b3f0eea1b0d908933cde2ae0
dasunR430 <128127298+dasunR430@users.noreply.github.com>
2025-08-15 09:14:40 +0530

    Update delete user to delete blogs when the user is deleted

--- src/controllers/v1/user/delete_user.ts
+++ src/controllers/v1/user/delete_user.ts
@@ -1,4 +1,9 @@
 /**
+ * Node Modules
+ */
+import { v2 as cloudinary } from 'cloudinary';
+
+/**
  * Custom Modules
  */
 import { logger } from "@/lib/winston";
@@ -7,6 +12,7 @@ import { logger } from "@/lib/winston";
  * Models
  */
 import User from "@/models/user";
+import Blog from "@/models/blog";
 
 /**
  * Types 
@@ -16,6 +22,22 @@ import { Request, Response } from "express";
 const deleteUser = async (req: Request,  res: Response): Promise<void> => {
     const userId = req.params.userId;
     try {
+        const blogs = await Blog.find({author: userId}).select('banner.publicId').lean().exec();
+        const publicIds = blogs.map(blog => blog.banner.publicId);
+
+        if(publicIds.length > 0) {
+            await cloudinary.api.delete_resources(publicIds);
+            logger.info(`Deleted ${publicIds.length} blog banner images from Cloudinary.`, {
+                publicIds
+            });
+        }
+
+        await Blog.deleteMany({author: userId});
+        logger.info(`Deleted ${blogs.length} blogs from the database.`, {
+            userId,
+            blogs
+        });
+
         await User.deleteOne({_id: userId});
         logger.info('A user account has been deleted', {
             userId
